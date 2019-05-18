a235fb8dace111315e1d21dd218585168155e781
Alexey Morozov <alexeymorozovv@yandex.ru>
2019-05-18 12:45:47 +0500

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,45 @@
+import './style.css';
+
+const canvas = document.createElement('canvas');
+document.body.style.backgroundColor = "#303030";
+
+
+canvas.width = 1024;
+canvas.height = 768;
+canvas.classList.add('game-display');
+
+
+const ctx = canvas.getContext('2d');
+if (ctx == null) {
+    throw Error("PNH");
+}
+
+document.body.appendChild(canvas);
+
+interface Dot {
+    x: number;
+    y: number;
+    radius: number;
+}
+
+canvas.onclick = (event: MouseEvent) => {
+    const [x, y] = [event.layerX, event.layerY];
+    const arc = arcs.find(a => (x <= a.x - a.radius || x >= a.x + a.radius) && (y <= a.y - a.radius || y >= a.y - a.radius));
+    if (arc) {
+        arc.radius = arc.radius * 1.1;
+    } else {
+        arcs.push({ x, y, radius: 20 });
+    }
+};
+
+const arcs: Array<Dot> = [];
+
+(function loop() {
+    arcs.forEach((arc) => {
+        ctx.beginPath();
+        ctx.fillStyle = '#3aebca';
+        ctx.arc(arc.x, arc.y, arc.radius, 0, Math.PI * 2);
+        ctx.fill();
+    });
+    requestAnimationFrame(loop);
+})();
---
+++ webpack.config.js
@@ -0,0 +1,45 @@
+const path = require('path');
+const HTMLPlugin = require('html-webpack-plugin');
+const webpack = require('webpack');
+
+module.exports = {
+    entry: './src/index.ts',
+    devtool: 'inline-source-map',
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                use: 'ts-loader',
+                exclude: [/node_modules/, /tests/]
+            },
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader'],
+            }
+        ]
+    },
+    resolve: {
+        extensions: [ '.tsx', '.ts', '.js' ]
+    },
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist')
+    },
+    plugins: [
+        new HTMLPlugin(),
+        new webpack.WatchIgnorePlugin([
+            path.join(__dirname, "node_modules")
+        ])
+    ],
+    devServer: {
+        // publicPath: 'public/',
+        watchOptions: {
+            ignored: [
+                path.resolve(__dirname, 'dist'),
+                path.resolve(__dirname, 'tests'),
+                path.resolve(__dirname, 'node_modules')
+            ]
+        }
+    },
+    mode: "development"
+};
