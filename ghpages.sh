1eff62b97211dd2df2991f74195c1a43976efcbb
Alexey Morozov <alexeymorozovv@yandex.ru>
2019-05-18 16:50:41 +0500

    Checkout master

--- ghpages.sh
+++ ghpages.sh
@@ -1,5 +1,11 @@
 # /bin/sh
 
-git checkout githubpage
+git checkout gh-pages
 git merge master
 yarn build
+mv dist/* .
+git add index.html bundle.js
+git commit -m 'Update gh-pages'
+git push
+git checkout master
+echo 'Done'
